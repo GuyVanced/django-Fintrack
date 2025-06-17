75548cf776f849d85707facc7f2b73d73e7db155
GuyVanced <pandeykushal017@gmail.com>
2025-06-17 15:46:18 +0545

    Added serializer file for monthly insights

---
+++ fintrack_app/serializers/monthlyInsights.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from fintrack_app.models.monthlyInsights import MonthlyInsight
+
+class MonthlyInsightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MonthlyInsight
+        fields = ['period_start', 'period_end', 'prompt_payload', 'llm_response', 'created_at']
